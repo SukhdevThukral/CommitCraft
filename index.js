d694629e8f6f593538fbeb20bf7a9773fa2cab6d
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-10-12 14:01:47 +0530

    feat: added a function to generate simple rule-based messages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const{execSync} = require("child_process");
 const readline = require("readline")
 
-function stagedFiles (){
+function getStagedFiles(){
     try{
         const output = execSync("git diff --staged --name-status").toString();
         const lines = output.trim().split("\n").filter(line => line);
@@ -13,4 +13,17 @@ function stagedFiles (){
         console.log("Error: make sure u have staged files bish.");
         process.exit(1)
     }
+}
+
+// func to gen simple msgs (rule based for now we will soon add ai to ts)
+function genMessage(stagedFiles){
+    const mesg = stagedFiles.map(f=>{
+        if (f.file.endsWith(".md")) return "docs: update" + f.file;
+        if (f.file.endsWith(".test.js")) return "test: update" + f.file;
+        if (f.status === 'A') return "feat:add" + f.file;
+        if (f.status === 'M') return "fix: update" + f.file;
+        if (f.status === 'D') return "chore: remove" + f.file;
+        return "chore: update" + f.file;
+    })
+    return mesg.join("\n");
 }
