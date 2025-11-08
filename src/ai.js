4ae1584e3951bd68551962ef9b07ebe2049f1c40
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-11-08 22:28:34 +0530

    chore: update test comment in genAIMessage function

--- src/ai.js
+++ src/ai.js
@@ -4,7 +4,7 @@ export async function genAIMessage(diff){
      if (!diff || !diff.trim()) {
         return "chore: update files";
     }
-    //test#1
+    //test(IM DYINGGG)
 
     if (!OPENROUTER_API_KEY) {
         throw new Error("missing api key in env variables.");
