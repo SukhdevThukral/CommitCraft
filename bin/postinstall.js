affaef96f09a9db9c8c51b8d012f63bd56099633
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-11-09 20:30:25 +0530

    chore: update postinstall.js comments for clarity
    - Changed comment from //test2 to //test1
    - Ensured consistent file format with no newline at end of file

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -4,4 +4,4 @@ if (!fs.existsSync(".env")){
     console.log("\n CommitCraft setup not complete!");
     console.log("Run `npx commitcraft-setup` to configure your API key. \n");
 }
-//test2
+//test1
