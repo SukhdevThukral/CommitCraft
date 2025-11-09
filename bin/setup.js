84bb60e1e3a984eb181f9add703b77c91fe3ed14
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-11-09 12:29:10 +0530

    feat: rename package to commitcraft-ai and update dependencies
    - Updated package name from commitcraft to commitcraft-ai.
    - Added new bin script for commitcraft-setup.
    - Included commitcraft-ai as a dependency in package-lock.json.

---
+++ bin/setup.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import fs from "fs";
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Enter your API Key: ", (key)=> {
+    if (!key) {
+      console.log("No key entered. Setup aborted.");
+      rl.close();
+      process.exit(1);  
+    }
+
+    let envContent = "";
+    if (fs.existsSync(".env")){
+      envContent = fs.readFileSync(".env", "utf-8");
+
+      //removin the old wuns
+      envContent= envContent.replace(/^OPENROUTER_API_KEY=.*$/m, "");
+    }
+
+    //addin new key
+    const newContent = envContent.trim() + `\nOPENROUTER_API_KEY=${key}\n`;
+    fs.writeFileSync(".env", newContent.trim() + "\n");
+    console.log("API key saved to .env successfully!");
+    rl.close();
+});
