cdbf8eb7be6d14ff771b17a6b15808bf4a4f3cd8
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-11-10 11:22:37 +0530

    feat: enhance postinstall script to check for global config file
    - Added check for global configuration file in user's home directory
    - Improved setup prompt when neither local env nor global config exist

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 import fs from "fs";
-if (!fs.existsSync(".env")){
+import os from "os";
+import path from "path";
+
+const localEnvPath = ".env";
+const globalConfigPath = path.join(os.homedir(), ".commitcraftsrc");
+
+if (!fs.existsSync(localEnvPath) && !fs.existsSync(globalConfigPath)){
     console.log("\n CommitCraft setup not complete!");
     console.log("Run `npx commitcraft-setup` to configure your API key. \n");
 }
