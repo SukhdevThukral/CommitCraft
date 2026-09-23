0cd9953a1e4da24a8bd40ccafd291a9c723dd375
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-09-23 23:58:56 +0530

    chore: remove unused node-fetch import

--- src/telemetry.js
+++ src/telemetry.js
@@ -2,7 +2,6 @@ import os from "os";
 import path from "path";
 import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
-import fetch from "node-fetch";
 
 const userIdFile = path.join(os.homedir(), "commitcraft_Id");
 
